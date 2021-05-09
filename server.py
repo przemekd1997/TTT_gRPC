e425ea8480637856d0c15c72d77f14be4e5b0b80
przemekd1997 <przemekd1997@gmail.com>
2021-05-09 17:05:15 +0200

    server now starting!

--- server.py
+++ server.py
@@ -1,21 +1,51 @@
 from fire import Base
+from concurrent import futures
 import grpc
 import ttt_service_pb2
 import ttt_service_pb2_grpc
+import time
+import threading
+
+free_games = []
 
 class Listener(ttt_service_pb2_grpc.TTTServicer):
     def __init__(self):
-        self.free_games = []
         self.data_base = Base("./titato-8a7f4-firebase-adminsdk-cacno-b118cf5928.json")
+        self.lock = threading.Lock()
 
     def JoinMatchmaking(self, request, context): 
+        global free_games
         uid = request.id
         if (len(free_games) == 0):
-            name = data_base.create_game(uid,True)
-            self.free_games.append(name)
+            name = self.data_base.create_game(uid,True)
+            with self.lock:
+                free_games.append(name)
+            while True:
+                print(free_games)
+                time.sleep(2)
+                if uid not in free_games:
+                    break
+   
         else:
-            name = self.free_games.pop(0)
-            data_base.add_to_game(uid,name)
+            with self.lock:
+                name = free_games.pop(0)
+            self.data_base.add_to_game(uid,name)
         game = ttt_service_pb2.Game(id = name)
-        self.free_games.append(game)
         yield game
+    
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    ttt_service_pb2_grpc.add_TTTServicer_to_server(Listener(), server)
+    server.add_insecure_port('[::]:9999')
+    server.start()
+    try:
+        while True:
+            print("server active: on threads %i" % (threading.active_count()))
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print("keyboard interrupt")
+        server.stop(0)
+
+if __name__ == "__main__":
+    serve()
